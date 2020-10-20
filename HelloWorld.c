42c84f5096f49a3934bf574c3a73d04d29529dde
Muhammad Syaiful Aliim <syaiful.id@gmail.com>
2020-10-20 09:25:59 +0700

    inisialisasi commit source code pertama kali

---
+++ HelloWorld.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : Muhammad Syaiful Aliim
+ Version     : 1.0.0
+ Copyright   : Copyleft
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("Muhammad Syaiful Aliim");
+	int a = 5;
+	int b = 4;
+	int c = a + b;
+	printf(""+c);
+	return EXIT_SUCCESS;
+}
